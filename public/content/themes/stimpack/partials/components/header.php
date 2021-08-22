5f7e97c748902819fd5c096e6a263fea72e0b50e
jdlxt <jdlxt@localhost>
2021-08-22 20:09:04 +0200

    sync

--- public/content/themes/stimpack/partials/components/header.php
+++ public/content/themes/stimpack/partials/components/header.php
@@ -22,6 +22,7 @@ $menuBackgroundColor = get_theme_mod('menu-background-color');
         <div class="profile">
             <img src="<?=$profileImage?>" alt="" class="img-fluid rounded-circle menu-profile-image">
             <h1 class="text-light menu-title"><a href="<?=get_home_url()?>"><?=$menuTitle?></a></h1>
+            <!--
             <div class="social-links mt-3 text-center">
                 <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
                 <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
@@ -29,6 +30,7 @@ $menuBackgroundColor = get_theme_mod('menu-background-color');
                 <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
                 <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
             </div>
+            //-->
         </div>
 
         <?php
